33e7babe0fb6410d19db045d33fbbb386ed78b6f
Pratiyush <pratiyush1@gmail.com>
2022-10-01 07:41:10 +0200

    String to UUID in Java vLog

---
+++ nullshots-reactive-spring/src/main/java/com/nullshots/NullshotsReactiveSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nullshots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullshotsReactiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullshotsReactiveSpringApplication.class, args);
+	}
+
+}
